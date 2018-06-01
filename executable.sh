da3a218a34966c55d100e245c7fdffcf0d56322a
Sarouar hossain <sarouarhossain@team-lab.com>
2018-06-01 14:18:54 +0900

    test5

--- executable.sh
+++ executable.sh
@@ -1,10 +1,13 @@
 pwd
 ls
-docker stop friendlyhello
-docker rm friendlyhello
+# docker stop friendlyhello
+# docker rm friendlyhello
 docker build -t friendlyhello .
 ./login.sh
 docker tag friendlyhello sarouar1/friendlyhello:part2
 docker image ls
 docker push sarouar1/friendlyhello:part2
-docker run -d --name friendlyhello -p 4000:80 friendlyhello
+# docker run -d --name friendlyhello -p 4000:80 friendlyhello
+docker swarm init
+docker stack deploy -c docker-compose.yml getstartedlab
+docker service ls
